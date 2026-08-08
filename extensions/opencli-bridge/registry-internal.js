861e084b45e6d4e835ad4358029d50a071515b78
juan-multi-7575 <juanmulti001@gmail.com>
2026-08-08 17:14:22 +0530

    restructure in progress

---
+++ extensions/opencli-bridge/registry-internal.js
@@ -0,0 +1,13 @@
+import { Strategy, cli, getRegistry } from '../../src/registry.ts';
+export { Strategy, cli, getRegistry };
+export {
+  CliError,
+  BrowserConnectError,
+  CommandExecutionError,
+  ConfigError,
+  ArgumentError,
+  EmptyResultError,
+  AuthRequiredError,
+  TimeoutError,
+  getErrorMessage
+} from '../../src/errors.ts';
